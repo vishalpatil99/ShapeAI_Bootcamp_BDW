a604fa88e40303c0f83eb8232dd1f5346509a6e9
vishalpatil99 <54531325+vishalpatil99@users.noreply.github.com>
2021-06-26 17:06:25 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="info">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        A basic web dev React js Bootcamp done under the guidance of shaurya
+        sinha sir.
+      </p>
+    </div>
+  );
+}
+export default Info;
